4f6b0e5524f2f2cab81faee0910e8a817708c24b
Anmol Tyagi <anmoltyagi675@gmail.com>
2025-12-23 22:23:28 +0530

    Update index.jsx

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Backend API runs on Express (see Backend/index.js -> PORT 3030)
-export const BASE_URL = "http://localhost:3030";
+export const BASE_URL = "https://linkdinwebsite.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
